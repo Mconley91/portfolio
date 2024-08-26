fdbc4b099c137f12d305334e3e07d83a4b813e52
Matt Conley <matthew.e.conley@gmail.com>
2024-08-26 17:15:28 -0400

    fixed type class colors being not applied or misapplied for the Pokedex app

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,24 +1,26 @@
+import { useState } from 'react'
 import './styles/Pokedex.css'
 
+
 export default function Pokedex(){
-    const inputElement = document.getElementById("search-input");
+    
     const searchButtonElement = document.getElementById("search-button");
-    const nameElement = document.getElementById("pokemon-name");
-    const idElement = document.getElementById("pokemon-id");
-    const weightElement = document.getElementById("weight");
-    const heightElement = document.getElementById("height");
-    const typesElement = document.getElementById("types");
-    const type1Element = document.getElementById("type1");
-    const type2Element = document.getElementById("type2");
-    const hpElement = document.getElementById("hp");
-    const attackElement = document.getElementById("attack");
-    const defenseElement = document.getElementById("defense");
-    const specialAttackElement = document.getElementById("special-attack");
-    const specialDefenseElement = document.getElementById("special-defense");
-    const speedElement = document.getElementById("speed");
-    const picture = document.getElementById("sprite");
-    let type1Value;
-    let type2Value;
+    const [nameElement, setNameElement] = useState("");
+    const [idElement,setIdElement] = useState("");
+    const [weightElement,setWeightElement] = useState("");
+    const [heightElement,setHeightElement] = useState("");
+    //const [typesElement,setTypesElement] = useState("");
+    const [hpElement,setHpElement] = useState("");
+    const [attackElement,setAttackElement] = useState("");
+    const [defenseElement,setDefenseElement] = useState("");
+    const [specialAttackElement,setSpecialAttackElement] = useState("");
+    const [specialDefenseElement,setSpecialDefenseElement] = useState("");
+    const [speedElement,setSpeedElement] = useState("");
+    const [picture,setPicture] = useState("");
+    let type1Element = document.getElementById('type1');
+    let type2Element= document.getElementById('type2');
+    //let type1;
+    //let type2;
 
     //event listeners are reading null and not working, changed to JSX element onClick property
     /*inputElement.addEventListener("keypress", (event)=>{
@@ -28,127 +30,121 @@ export default function Pokedex(){
     });
     searchButtonElement.addEventListener("click", ()=> search()); */
 
-    const search = async function() {
-    nameElement.innerText = "";
-    idElement.innerText = "";
-    weightElement.innerText = ""; 
-    heightElement.innerText = "";
-    type1Element.innerText = "";
-    type1Element.className = "";
-    type2Element.innerText = "";
-    type2Element.className = "";
-    hpElement.innerText = "";
-    attackElement.innerText = "";
-    defenseElement.innerText = "";
-    specialAttackElement.innerText = "";
-    specialDefenseElement.innerText = "";
-    speedElement.innerText = "";
-    picture.src = "";
+async function search() {
+    const inputElement = document.getElementById("search-input")
     const response = await fetch("https://pokeapi-proxy.freecodecamp.rocks/api/pokemon");
     const data = await response.json();
     //added reverse name detection per cynthia's request
     const foundPokemon = data.results.find((i)=>i.name === inputElement.value.toLowerCase() || i.id === Number(inputElement.value) || i.name === inputElement.value.split('').reverse().join('').toLowerCase());
     if (foundPokemon){
-        nameElement.innerText = foundPokemon.name.charAt(0).toUpperCase()+ foundPokemon.name.slice(1);
-        idElement.innerText = foundPokemon.id;
+        setNameElement(foundPokemon.name.charAt(0).toUpperCase() + foundPokemon.name.slice(1));
+        setIdElement(foundPokemon.id);
         const response2 = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${foundPokemon.name}`);
         const data2 = await response2.json();
-        weightElement.innerText = data2.weight; 
-        heightElement.innerText = data2.height;
+        setWeightElement(data2.weight); 
+        setHeightElement(data2.height);
         //type logic block
-        type1Value = data2.types[0].type.name.toUpperCase();
-        type1Element.innerText = type1Value;
-        typeColors(type1Element);
-        if(data2.types[1]){
-        type2Value = data2.types[1].type.name.toUpperCase();
-        type2Element.innerText = type2Value;
-        typeColors(type2Element);
+        const type1 = data2.types[0].type.name.toUpperCase(); 
+        type1Element.innerText = type1;
+        typeColors(type1);
+        if(data2.types[1]){ 
+        const type2 = data2.types[1].type.name.toUpperCase();
+        type2Element.innerText = type2
+        typeColors(type2);
+        }
+        else{
+            type2Element.innerText = ""
+            type2Element.className= "";
         };
-        //
-        hpElement.innerText = data2.stats[0].base_stat;
-        attackElement.innerText = data2.stats[1].base_stat;
-        defenseElement.innerText = data2.stats[2].base_stat;
-        specialAttackElement.innerText = data2.stats[3].base_stat;
-        specialDefenseElement.innerText = data2.stats[4].base_stat;
-        speedElement.innerText = data2.stats[5].base_stat;
-        picture.src = data2.sprites.front_default;
+        setHpElement(data2.stats[0].base_stat);
+        setAttackElement(data2.stats[1].base_stat);
+        setDefenseElement(data2.stats[2].base_stat);
+        setSpecialAttackElement(data2.stats[3].base_stat);
+        setSpecialDefenseElement(data2.stats[4].base_stat);
+        setSpeedElement(data2.stats[5].base_stat);
+        setPicture(data2.sprites.front_default);
     }
     else {
         alert("Pokémon not found")
     };
-    };
-
-    const typeColors = (element)=>{
-        let type;
-        if(element === type1Element){
-        type = type1Value;
+};
+ //working here: HTML elements are rendering after JSX resulting in null values
+    function typeColors(type) {
+        let currentElement;
+        if(type === type1Element.innerText){
+            currentElement = type1Element;
+            classAssigner(currentElement);
         }
-        else{
-        type = type2Value;
+        else if(type === type2Element.innerText){ 
+            currentElement = type2Element;
+            classAssigner(currentElement);
         };
-        element.classList.add("type");
-        switch (type){
-        case "NORMAL":
-            element.classList.add("normal");
-        break;
-        case "FIGHTING":
-            element.classList.add("fighting");
-        break;
-        case "FLYING":
-            element.classList.add("flying");
-        break;
-        case "POISON":
-            element.classList.add("poison");
-        break;
-        case "GROUND":
-            element.classList.add("ground");
-        break;
-        case "ROCK":
-            element.classList.add("rock");
-        break;
-        case "BUG":
-            element.classList.add("bug");
-        break;
-        case "GHOST":
-            element.classList.add("ghost");
-        break;
-        case "STEEL":
-            element.classList.add("steel");
-        break;
-        case "FIRE":
-            element.classList.add("fire");
-        break;
-        case "WATER":
-            element.classList.add("water");
-        break;
-        case "GRASS":
-            element.classList.add("grass");
-        break;
-        case "ELECTRIC":
-            element.classList.add("electric");
-        break;
-        case "PSYCHIC":
-            element.classList.add("psychic");
-        break;
-        case "ICE":
-            element.classList.add("ice");
-        break;
-        case "DRAGON":
-            element.classList.add("dragon");
-        break;
-        case "DARK":
-            element.classList.add("dark");
-        break;
-        case "FAIRY":
-            element.classList.add("fairy");
-        break;
-        case "STELLAR":
-            element.classList.add("stellar");
-        break;
-        case "???":
-            element.classList.add("unknown");
-        break;
-        }
+        function classAssigner(currentElement){
+            currentElement.className= "";
+            currentElement.classList.add("type");
+            switch (currentElement.innerText){
+            case "NORMAL":
+                currentElement.classList.add("normal");
+            break;
+            case "FIGHTING":
+                currentElement.classList.add("fighting");
+            break;
+            case "FLYING":
+                currentElement.classList.add("flying");
+            break;
+            case "POISON":
+                currentElement.classList.add("poison");
+            break;
+            case "GROUND":
+                currentElement.classList.add("ground");
+            break;
+            case "ROCK":
+                currentElement.classList.add("rock");
+            break;
+            case "BUG":
+                currentElement.classList.add("bug");
+            break;
+            case "GHOST":
+                currentElement.classList.add("ghost");
+            break;
+            case "STEEL":
+                currentElement.classList.add("steel");
+            break;
+            case "FIRE":
+                currentElement.classList.add("fire");
+            break;
+            case "WATER":
+                currentElement.classList.add("water");
+            break;
+            case "GRASS":
+                currentElement.classList.add("grass");
+            break;
+            case "ELECTRIC":
+                currentElement.classList.add("electric");
+            break;
+            case "PSYCHIC":
+                currentElement.classList.add("psychic");
+            break;
+            case "ICE":
+                currentElement.classList.add("ice");
+            break;
+            case "DRAGON":
+                currentElement.classList.add("dragon");
+            break;
+            case "DARK":
+                currentElement.classList.add("dark");
+            break;
+            case "FAIRY":
+                currentElement.classList.add("fairy");
+            break;
+            case "STELLAR":
+                currentElement.classList.add("stellar");
+            break;
+            case "???":
+                currentElement.classList.add("unknown");
+            break;
+            }
+        };
     };
     return(
     <main id="pokedex">
@@ -157,24 +153,24 @@ export default function Pokedex(){
         <button id="search-button" onClick={search}>Search</button>
       </div>
       <div id="readout">
-        <img src="" id="sprite"/>
-        <div className="item"><span>Name:</span> <span className="attributes" id="pokemon-name"></span></div>
-        <div className="item"><span>ID: </span><span className="attributes" id="pokemon-id"></span></div>
-        <div className="item"><span>Weight: </span><span className="attributes" id="weight"></span></div>
-        <div className="item"><span>Height: </span><span className="attributes" id="height"></span></div>
+        <img src={picture} id="sprite"/>
+        <div className="item"><span>Name:</span> <span className="attributes" id="pokemon-name">{nameElement}</span></div>
+        <div className="item"><span>ID: </span><span className="attributes" id="pokemon-id">{idElement}</span></div>
+        <div className="item"><span>Weight: </span><span className="attributes" id="weight">{weightElement}</span></div>
+        <div className="item"><span>Height: </span><span className="attributes" id="height">{heightElement}</span></div>
         <div className="item"><span>Types: </span>
           <span className="attributes" id="types">
             <span id="type1"></span>
             <span id="type2"></span>
           </span>
         </div>
-        <div className="item"><span>HP: </span><span className="attributes" id="hp"></span></div>
-        <div className="item"><span>Attack: </span><span className="attributes" id="attack"></span></div>
-        <div className="item"><span>Defense: </span><span className="attributes" id="defense"></span></div>
-        <div className="item"><span>SA: </span><span className="attributes" id="special-attack"></span></div>
-        <div className="item"><span>SD: </span><span className="attributes" id="special-defense"></span></div>
-        <div className="item"><span>Speed: </span><span className="attributes" id="speed"></span></div>
+        <div className="item"><span>HP: </span><span className="attributes" id="hp">{hpElement}</span></div>
+        <div className="item"><span>Attack: </span><span className="attributes" id="attack">{attackElement}</span></div>
+        <div className="item"><span>Defense: </span><span className="attributes" id="defense">{defenseElement}</span></div>
+        <div className="item"><span>SA: </span><span className="attributes" id="special-attack">{specialAttackElement}</span></div>
+        <div className="item"><span>SD: </span><span className="attributes" id="special-defense">{specialDefenseElement}</span></div>
+        <div className="item"><span>Speed: </span><span className="attributes" id="speed">{speedElement}</span></div>
       </div>
     </main>
     )
-}
+};
